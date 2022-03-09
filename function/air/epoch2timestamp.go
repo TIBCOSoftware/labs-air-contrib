fb2d9ea1b1a6b8fa63629f14b20f5081f90a07f2
Infospecktra <Infospecktra@gmail.com>
2022-03-09 16:00:47 -0500

    New Feature
    Adding function to convert epoch time to string timestamp

---
+++ function/air/epoch2timestamp.go
@@ -0,0 +1,37 @@
+package air
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/project-flogo/core/data"
+	"github.com/project-flogo/core/data/expression/function"
+)
+
+func init() {
+	function.Register(&fnEpoch2Timestamp{})
+}
+
+type fnEpoch2Timestamp struct {
+}
+
+func (fnEpoch2Timestamp) Name() string {
+	return "epoch2timestamp"
+}
+
+func (fnEpoch2Timestamp) Sig() (paramTypes []data.Type, isVariadic bool) {
+	return []data.Type{data.TypeString}, false
+}
+
+func (fnEpoch2Timestamp) Eval(params ...interface{}) (interface{}, error) {
+	/* quick and dirty : only for realtime scenario */
+	log.Info("(fnEpoch2Timestamp.Eval) params[0] : ", params[0], ", params[1] : ", params[1])
+	epoch := params[0].(int64)
+	if epoch > 1000000000000000000 {
+		epoch = epoch / 1000000000
+	} else if epoch > 1000000000000 {
+		epoch = epoch / 1000
+	}
+
+	return time.Unix(epoch, 0).Format(params[1].(string)), nil
+}
