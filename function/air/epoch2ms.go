c67b3ff3bd7793b93be1711f1c24824ed2291f3e
Infospecktra <Infospecktra@gmail.com>
2022-03-30 17:22:10 -0400

    New Feature
    Convert epoch ms, us, ns to ms

---
+++ function/air/epoch2ms.go
@@ -0,0 +1,36 @@
+package air
+
+import (
+	"time"
+
+	"github.com/project-flogo/core/data"
+	"github.com/project-flogo/core/data/expression/function"
+)
+
+func init() {
+	function.Register(&fnEpoch2ms{})
+}
+
+type fnEpoch2ms struct {
+}
+
+func (fnEpoch2ms) Name() string {
+	return "epoch2ms"
+}
+
+func (fnEpoch2ms) Sig() (paramTypes []data.Type, isVariadic bool) {
+	return []data.Type{data.TypeInt}, false
+}
+
+func (fnEpoch2ms) Eval(params ...interface{}) (interface{}, error) {
+	/* quick and dirty : only for realtime scenario */
+	log.Info("(fnEpoch2ms.Eval) params[0] : ", params[0], ", params[1] : ", params[1])
+	epoch := int64(params[0].(int))
+	if epoch > 1000000000000000000 {
+		epoch = epoch / 1000000000
+	} else if epoch > 1000000000000 {
+		epoch = epoch / 1000
+	}
+
+	return epoch, nil
+}
